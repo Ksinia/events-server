efa94958de569bfca586ba3d04e898b2dd38ce41
Ksinia <xiatian@mail.ru>
2019-12-07 19:16:48 +0100

    pagination

--- event/router.js
+++ event/router.js
@@ -7,11 +7,22 @@ router.post("/event", (req, res, next) => {
     .then(event => res.send(event))
     .catch(error => next(error));
 });
+// router.get("/event", (req, res, next) => {
+//   Event.findAll()
+//     .then(events => res.send(events))
+//     .catch(error => next(error));
+// });
+
+//pagination
 router.get("/event", (req, res, next) => {
-  Event.findAll()
-    .then(events => res.send(events))
+  const limit = Math.min(req.query.limit || 25, 500);
+  const offset = req.query.offset || 0;
+
+  Event.findAndCountAll({ limit, offset })
+    .then(result => res.send({ events: result.rows, total: result.count }))
     .catch(error => next(error));
 });
+
 router.get("/event/:id", (req, res, next) => {
   Event.findByPk(req.params.id)
     .then(event => res.send(event))
